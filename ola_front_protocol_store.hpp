7899dd7e6490cdba1ad3e531daa352855d3e9be1
Palade Valentin <vipalade@gmail.com>
2021-03-04 11:25:12 +0200

    Reflection (#2)
    * start migrating to reflection
    
    * reflection: keep working
    
    * reflection: ola server full compilation
    
    * reflection: more refactoring
    
    * reflection: clang format

--- ola_front_protocol_store.hpp
+++ ola_front_protocol_store.hpp
@@ -1,33 +1,91 @@
 #pragma once
-#include "ola/common/ola_front_protocol_common.hpp"
+#include "ola/common/ola_front_protocol_main.hpp"
 
 namespace ola {
 namespace front {
+namespace store {
+constexpr uint8_t protocol_id = 3;
 
-struct AcquireAppRequest : solid::frame::mprpc::Message {
-    static constexpr uint32_t version = 1;
+//the version is only transfered from client to server.
+//the client will NOT know the server version
+struct Version {
+    static constexpr uint32_t version             = 1;
+    static constexpr uint32_t acquire_app_request = 1;
+    static constexpr uint32_t init_request        = 1;
 
-    uint32_t    version_ = version;
-    std::string app_id_;
-    bool        acquire_ = true;
+    uint32_t version_             = version;
+    uint32_t acquire_app_request_ = acquire_app_request;
+    uint32_t init_request_        = init_request;
 
-    SOLID_PROTOCOL_V2(_s, _rthis, _rctx, _name)
+    void clear()
     {
-        solid::serialization::addVersion<AcquireAppRequest>(_s, _rthis.version_, "version");
+        acquire_app_request_ = std::numeric_limits<uint32_t>::max();
+    }
 
-        _s.add([&_rthis](S& _s, solid::frame::mprpc::ConnectionContext& _rctx, const char* /*_name*/) {
-            _s.add(_rthis.app_id_, _rctx, "app_id");
-            _s.add(_rthis.acquire_, _rctx, "acquire");
+    bool operator<=(const Version& _rthat) const
+    {
+        return version_ <= _rthat.version_ && acquire_app_request_ <= _rthat.acquire_app_request_ && init_request_ <= _rthat.init_request_;
+    }
+
+    SOLID_REFLECT_V1(_s, _rthis, _rctx)
+    {
+        _s.add(_rthis.version_, _rctx, 0, "version");
+        _s.add([&_rthis](Reflector& _s, Context& _rctx) {
+            if constexpr (!Reflector::is_const_reflector) {
+                if (_rthis.version > Version::version) {
+                    _rthis.clear();
+                    return;
+                }
+            }
+            if (_rthis.version_ == version) {
+                _s.add(_rthis.acquire_app_request_, _rctx, 2, "acquire_app_request");
+                _s.add(_rthis.init_request_, _rctx, 3, "init_request");
+            }
         },
-            _rctx, _name);
+            _rctx);
     }
 };
 
-template <class R>
-inline void protocol_setup_store(R _r, ProtocolT& _rproto)
+constexpr Version version;
+
+struct InitRequest : solid::frame::mprpc::Message {
+    Version          store_version_;
+    core::Version    core_version_;
+    main::Version    main_version_;
+    utility::Version utility_version_;
+
+    SOLID_REFLECT_V1(_s, _rthis, _rctx)
+    {
+        _s.add(_rthis.store_version_, _rctx, 1, "store_version");
+        _s.add([&_rthis](Reflector& _s, Context& _rctx) {
+            if (_rthis.store_version_.init_request_ == Version::init_request) {
+                _s.add(_rthis.core_version_, _rctx, 3, "core_version");
+                _s.add(_rthis.main_version_, _rctx, 4, "main_version");
+                _s.add(_rthis.utility_version_, _rctx, 5, "utility_version");
+            }
+        },
+            _rctx);
+    }
+};
+
+struct AcquireAppRequest : solid::frame::mprpc::Message {
+    std::string app_id_;
+    bool        acquire_ = true;
+
+    SOLID_REFLECT_V1(_s, _rthis, _rctx)
+    {
+        _s.add(_rthis.app_id_, _rctx, 1, "app_id");
+        _s.add(_rthis.acquire_, _rctx, 2, "acquire");
+    }
+};
+
+template <class Reg>
+inline void configure_protocol(Reg _rreg)
 {
-    _r(_rproto, solid::TypeToType<AcquireAppRequest>(), {3, 1});
+    _rreg({protocol_id, 1}, "InitRequest", solid::TypeToType<InitRequest>());
+    _rreg({protocol_id, 2}, "AcquireAppRequest", solid::TypeToType<AcquireAppRequest>());
 }
 
+} //namespace store
 } //namespace front
 } //namespace ola
